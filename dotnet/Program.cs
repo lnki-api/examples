cece313eb907528e758fd7089ec6b8483267b2c1
lnki-io <52611426+lnki-io@users.noreply.github.com>
2019-07-06 16:51:08 -0500

    Update Program.cs

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -42,7 +42,7 @@ namespace lnkisample
             client.BearerToken = apikey;
 
             var req = new shortenLinkRequest();
-            req.url = "https://maps.google.com";
+            req.url = "https://www.google.com/maps/place/Fremont+Troll/@47.6496755,-122.3646033,14.01z/data=!4m5!3m4!1s0x5490150128a784bd:0x9ddb04f1ce7199df!8m2!3d47.6512453!4d-122.3476821";
             req.lifetime_days = 5;
             req.sms_message = "Hello from {{url}}";
             req.sms_numbers = new List<string> { { "5555551212" } };
