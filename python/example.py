1c79c45af84a0bd6f7667c372b473f0bfacd5530
lnki-io <52611426+lnki-io@users.noreply.github.com>
2019-07-06 16:50:17 -0500

    Update example.py

--- python/example.py
+++ python/example.py
@@ -19,7 +19,7 @@ apikey = 'Bearer ' + os.environ['LNKI_IO_API_KEY']
 request = shortenLinkRequest()
 request.lifetime_days = 5
 request.sms_message = "Hello from {{url}}"
-request.url = "https://www.google.com/maps/dir/Seattle,+WA"
+request.url = "https://www.google.com/maps/place/Fremont+Troll/@47.6496755,-122.3646033,14.01z/data=!4m5!3m4!1s0x5490150128a784bd:0x9ddb04f1ce7199df!8m2!3d47.6512453!4d-122.3476821"
 
 # *** CHANGE THIS TO YOUR PHONE NUMBER
 request.sms_numbers = ["5555551212"]
